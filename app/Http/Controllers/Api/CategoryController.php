d360da1e0c86911e4d735bc245a85861d38be58d
Nour Muhammed <nourmuhammed20121994@gmail.com>
2024-06-08 10:32:46 +0300

    description en

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -107,7 +107,7 @@ class CategoryController extends Controller
                 ->orderBy('vip', 'ASC')
                 ->orderBy('special', 'ASC')
                 ->orderBy(DB::raw('ISNULL(provider_category_arrange), provider_category_arrange'), 'ASC')
-                ->paginate(50);
+                ->paginate(10);
         } elseif ($request->latitude and $request->longitude and $request->rate_order == null) {
             $providers = Provider::with('provider_categories', 'city')
                 ->whereHas('city', function ($q) use ($request) {
@@ -132,7 +132,7 @@ class CategoryController extends Controller
                 ->orderBy('vip', 'ASC')
                 ->orderBy('special', 'ASC')
                 ->orderBy(DB::raw('ISNULL(provider_category_arrange), provider_category_arrange'), 'ASC')
-                ->paginate(50);
+                ->paginate(10);
         } elseif ($request->latitude == null and $request->rate_order) {
             $providers = Provider::with('provider_categories', 'city')
                 ->whereHas('city', function ($q) use ($request) {
@@ -157,7 +157,7 @@ class CategoryController extends Controller
                 ->orderBy('vip', 'ASC')
                 ->orderBy('special', 'ASC')
                 ->orderBy(DB::raw('ISNULL(provider_category_arrange), provider_category_arrange'), 'ASC')
-                ->paginate(50);
+                ->paginate(10);
         } else {
             $providers = Provider::with('provider_categories', 'city')
                 ->whereHas('city', function ($q) use ($request) {
@@ -178,7 +178,7 @@ class CategoryController extends Controller
                 ->orderBy('vip', 'ASC')
                 ->orderBy('special', 'ASC')
                 ->orderBy(DB::raw('ISNULL(provider_category_arrange), provider_category_arrange'), 'ASC')
-                ->paginate(50);
+                ->paginate(10);
         }
         if ($providers->count() > 0) {
             return ApiController::respondWithSuccessData(new ProviderCollectionTest($providers));
@@ -212,7 +212,7 @@ class CategoryController extends Controller
             ->orderBy('vip', 'ASC')
             ->orderBy('special', 'ASC')
             ->orderBy(DB::raw('ISNULL(provider_category_arrange), provider_category_arrange'), 'ASC')
-            ->paginate(50);
+            ->paginate(10);
         if ($providers->count() > 0) {
             return ApiController::respondWithSuccessData(new ProviderCollectionTest($providers));
         } else {
